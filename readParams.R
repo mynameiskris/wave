c01fc15111aafbc36df430dfed2dbeec7ae6b69c
amit <amit.verma13@gmail.com>
2019-07-09 12:20:18 -0400

    first commit

---
+++ readParams.R
@@ -0,0 +1,37 @@
+readParams = function(filename) {
+  para <- list()
+  Y <- readLines(filename)
+  
+  para$title <- strsplit(Y[grep("Title",Y)],',')[[1]][2]
+  para$sim <- as.numeric(strsplit(Y[grep("Number of simulations",Y)],',')[[1]][2])
+  para$rseed <- as.numeric(strsplit(Y[grep("Seed",Y)],',')[[1]][2])
+  para$all_or_no_vaccine <- as.numeric(strsplit(Y[grep("Leaky or all-or-none vaccine",Y)],',')[[1]][2])
+  
+  para$inputsfile <- ifelse(strsplit(Y[grep("Input and calculated parameters",Y)],',')[[1]][2]=="yes",TRUE,FALSE) 
+  para$detailed_file <- ifelse(strsplit(Y[grep("Detailed for each simulation",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$daily_each_sim_file <- ifelse(strsplit(Y[grep("Incidence-daily for each simulation",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$period_each_sim_file <- ifelse(strsplit(Y[grep("Incidence-period for each simulation",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$seasonal_each_sim_file <- ifelse(strsplit(Y[grep("Incidence-total for each simulation",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$daily_overall_file <- ifelse(strsplit(Y[grep("Incidence-daily means over all simulations",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$monthly_overall_file <- ifelse(strsplit(Y[grep("Incidence-monthly means over all simulations",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$seasonal_overall_file <- ifelse(strsplit(Y[grep("Incidence-total means over all simulations",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$population_report_file <- ifelse(strsplit(Y[grep("Outcomes for each simulation",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  para$timestamp <- ifelse(strsplit(Y[grep("Add timestamp to output file names",Y)],',')[[1]][2]=="yes",TRUE,FALSE)
+  
+  para$NDJ <- as.numeric(strsplit(Y[grep("Number of days in each period",Y)],',')[[1]][2])
+  para$NJ <- as.numeric(strsplit(Y[grep("Number of periods in the study",Y)],',')[[1]][2])
+  para$ND <- as.numeric(strsplit(Y[grep("Number of days in the study",Y)],',')[[1]][2])
+  if (para$ND != para$NDJ * para$NJ) {
+    stop("Number of days in the study isn't a multiple of number of periods")
+  }
+  para$N <- as.numeric(strsplit(Y[grep("Number of participants in study",Y)],',')[[1]][2])
+  para$pai <- as.numeric(strsplit(Y[grep("Probability of X=1",Y)],',')[[1]][2])
+  para$alpha_1 <- as.numeric(strsplit(Y[grep("Probability of vaccination for X=1",Y)],',')[[1]][2])
+  para$alpha_0 <- as.numeric(strsplit(Y[grep("Probability of vaccination for X=0",Y)],',')[[1]][2])
+  para$beta_d01 <- as.numeric(strsplit(Y[grep("Daily probability of infection for V=0 X=1",Y)],',')[[1]][2])
+  para$theta_dx <- as.numeric(strsplit(Y[grep("Multiplier for daily probability of infection for V=1",Y)],',')[[1]][2])
+  para$theta <- as.numeric(strsplit(Y[grep("Multiplier for daily probability of infection for X=0",Y)],',')[[1]][2])
+  
+  
+  return(para)
+}
---
+++ simvee.R
@@ -0,0 +1,90 @@
+
+
+# Source functions from other files
+source("readParams.R")
+set.seed(1234)
+# Read parameters from input files
+params <- readParams("SimVE_input.csv")
+
+## Filenames
+#output_files = c('Outcomes.csv', 'detailed.csv')
+
+ID <- seq(1, params$N)
+X <-  rep(0, params$N)
+V <-  rep(0, params$N)
+DINF <-  rep(0, params$N)
+
+subject <- data.frame(ID=ID, X=X, V=V, DINF=DINF)
+
+## SubjectY goes from d=0 to d=ND
+subjectY <- matrix(NA, nrow=params$N, ncol=params$ND+1)
+
+NDINF = array(0, dim=c(2, 2, params$ND))
+
+### Initialize
+subjectY[,1] <- rep(0, params$N) 
+
+# Day
+d = 0
+
+params$beta_d11 = params$beta_d01 * params$theta_dx
+params$beta_d00 = params$beta_d01 * params$theta
+params$beta_d10 = params$beta_d01 * params$theta_dx * params$theta
+
+
+## Set value of X for each subject
+subject$X = as.numeric(runif(params$N) < params$pai) 
+
+## Set vaccination status for each subject
+for (i in ID) {
+  if (subject[i,"X"] == 0) {
+    subject[i,"V"] = as.numeric(runif(1) < params$alpha_0)
+  } 
+  if (subject[i,"X"] == 1) {
+    subject[i,"V"] = as.numeric(runif(1) < params$alpha_1)
+  }
+}
+
+## This function returns the beta value that should be applied to a
+#  subject based on X and Y values.
+getBetaForSubject = function (sub) {
+  X = sub$X
+  V = sub$V
+  if (X == 0 && V == 0) return(params$beta_d00)
+  if (X == 0 && V == 1) return(params$beta_d01)
+  if (X == 1 && V == 0) return(params$beta_d10)
+  if (X == 1 && V == 1) return(params$beta_d11)
+}
+
+
+## Iterate over number of days.
+while (d < params$ND) {
+  d = d + 1
+  
+  for (i in ID) {
+    if (subjectY[i, d] == 0) {
+      subjectY[i, (d+1)] = as.numeric(runif(1) < getBetaForSubject(subject[i,])) 
+      if (subjectY[i, (d+1)] == 1) {
+        subject[i, "DINF"] = (d + 1)
+        NDINF[subject[i,"X"], subject[i,"V"], d] = NDINF[subject[i,"X"], subject[i,"V"], d] + 1
+      }
+    }
+    if (subjectY[i, d] == 1) {
+      subjectY[i, (d+1)] = 2
+    }
+    if (subjectY[i, d] == 2) {
+      subjectY[i, (d+1)] = 2
+    }
+  }
+}
+
+
+
+if (params$population_report_file == TRUE) {
+  write.csv(subject, 'Outcomes.csv', row.names = FALSE)
+}
+if (params$detailed_file == TRUE) {
+  write.csv(cbind(subject, subjectY), 'Detailed.csv', row.names = FALSE)
+}
+
+
