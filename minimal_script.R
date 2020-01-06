aca1b6dc164d047d4342cad53c5875456853393a
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2020-01-06 13:59:08 +0000

    minimal script for running simulation

---
+++ minimal_script.R
@@ -0,0 +1,16 @@
+# SIMVEE minimal script
+
+library(dplyr)
+# Source functions from other files
+source('simvee.R')
+source("readParams.R")
+
+set.seed(1234)
+# Read parameters from input files
+params <- readParams("SimVEE_input.csv")
+
+# run simulation
+mysims <- run_sim(params)
+
+# write output to file
+write_output(params, mysims, path = "output/")
