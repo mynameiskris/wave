dba0dd935f9149ecaed35b9b3ac03abfaa220985
kylieainslie <ainslie.kylie@gmail.com>
2020-02-21 17:07:04 +0000

    update output from durham_ve and tian_ve average VE over all simulations

--- R/ve_methods.R
+++ R/ve_methods.R
@@ -35,11 +35,10 @@ durham_ve <- function(x, df = 2, nsmo = 40,var,...){
   yr <- range(yhat, yup, ylow)
   
   # output  
-  rtn <- tibble(variable = rep(var,length(pred.x)),
-                pred_x = round(pred.x), 
-                y_hat = as.numeric(yhat),
-                y_lower = as.numeric(ylow),
-                y_upper = as.numeric(yup))
+  rtn <- tibble(time = round(pred.x), 
+                ve = as.numeric(yhat),
+                ve_lower = as.numeric(ylow),
+                ve_upper = as.numeric(yup))
   return(rtn)
 }
 
@@ -79,28 +78,45 @@ ferdinands_ve <- function(dat, splines = FALSE){
 # Estimate VE using method from Tian et al. 2005 
 tian_ve <- function(dat, n_timepoint_breaks = 40){
   # fit timecox model
-  fit = timecox(Surv(DINF, FARI) ~ V, data = dat, n.sim = 500, max.time = 700)
+  fit = timecox(Surv(DINF_new, FARI) ~ V, data = dat, n.sim = 500, max.time = 700)
   # cummulative estimates
-  alpha0 = fit$cum[,2]
-  betas.cumul = fit$cum[,-(1:2)]
-  betas.cumul.var = fit$var.cum
+  # rtn_cum <- tibble(alpha0 = fit$cum[,2], beta_t = fit$cum[,-(1:2)]) %>% 
+  #               mutate(ve = 1 - exp(beta_t))
   # de-cummulative estimates
-  predicted.timepts <- seq(from = min(fit$cum[,1]), to = max(fit$cum[,1]), length = n_timepoint_breaks)
-  decumulated.ests = CsmoothB(fit$cum, predicted.timepts, b = 1)
-  timecox.hazardrate = exp(decumulated.ests[,2])
-  timecox.var = decumulated.ests[,-(1:2)]
+    predicted.timepts <- seq(from = min(fit$cum[,1]), to = max(fit$cum[,1]), length = n_timepoint_breaks)
+    decumulated.ests = CsmoothB(fit$cum, predicted.timepts, b = 1)
+    timecox.hazardrate = exp(decumulated.ests[,2])
+    timecox.var = decumulated.ests[,-(1:2)]
+    rtn <- tibble(time = predicted.timepts,hazardrate = timecox.hazardrate) %>% 
+              mutate(ve = 1 - hazardrate)
   # output
-  rtn_cum <- tibble(alpha0 = alpha0, beta_t = betas, beta_t_var = betas.cumul.var)
-  rtn_decum <- tibble(time = predicted.timepts, 
-                hazardrate = timecox.hazardrate,
-                lower = timecox.hazardrate - 1.96*sqrt(timecox.var),
-                upper = timecox.hazardrate + 1.96*sqrt(timecox.var))
-  return(list(cummulative = rtn_cum,
-              decumulative = rtn_decum))
+  return(rtn)
 }
 
 # Estimate VE using method from Ainslie et al. 2017 (SIM)
-ainslie_ve <- function(dat){
+# ainslie_ve <- function(dat, n_days){
+#   
+#   N <- length(unique(dat$ID))
+#   prev <- numeric(n_days)
+#   for (d in 1:n_days){
+#     prev[d] <- length(which(dat$DINF == d))/N
+#   }
+#   prev <- ifelse(prev == 0, 0.0001, prev)
+#   
+#   logLik <- function(vac_status, prev){
+#     # conditional probabilities
+#       pi_00 <- (1 - gamma_0) * prev
+#       pi_1 <- 1 - ((1 - gamma_v) * prev)
+#     # unconditional probabilities
+#       phi_0[d] <- pi_0 * phi_0[d-1]
+#       phi_1[d] <- pi_1 * phi_0[d-1]
+#     
+#     L = 
+#     -sum(log(L))
+#   }
+#   
+#   mle <- optim(par = 0.1, logLik, method = "L-BFGS-B", lower = 0, upper = 1, hessian = TRUE)
+#   
   
-}
+# }
 
--- scripts/minimal_script.R
+++ scripts/minimal_script.R
@@ -1,5 +1,4 @@
 # SIMVEE minimal script
-
 library(dplyr)
 library(foreign)
 library(survival)
@@ -8,8 +7,8 @@ library(timereg)
 library(ggplot2)
 # Source functions from other files
 source('simvee.R')
-source("readParams.R")
-
+source('readParams.R')
+source('ve_methods.R')
 # Read parameters from input files
 # you can specify the folder and file names of the infput file within the ""
 params <- readParams("input/SimVEE_input__Test_04.csv")
@@ -20,43 +19,62 @@ params <- readParams("input/SimVEE_input__Test_04.csv")
 mysims <- run_simvee(params)
 
 ### read in outcomes file
-outcomes_dat <- read.csv("output/Outcomes_Test_4.csv")
+# you can specify the file name of the output file inside ""
+outcomes_dat <- read.csv("Outcomes__Test_03.csv")
 # add FARI indicator variable
 outcomes_dat <- outcomes_dat %>% mutate(FARI = ifelse(DINF == 0, 0, 1),
                                         DINF_new = ifelse(DINF == 0, 999, DINF))
 
-### apply method from Durham et al. 1988
-source('R/ve_methods.R')
-# loop through simulations
+### apply VE estimation methods 
+
+# loop through simulations and apply each method
 for (i in 1:max(outcomes_dat$Sim)){
+  # subset data for each simulation
   outcomes_dat1 <- outcomes_dat %>% filter(Sim == i)
+  ######################################
+  ### method from Durham et al. 1988 ###
+  ######################################
   # fit ordinary Cox propotional hazards model
     flu_coxmod <- coxph(Surv(DINF_new,FARI) ~ V, data=outcomes_dat1)
   # test the proportional hazards assumption and compute the Schoenfeld residuals ($y)
     flu_zph <- cox.zph(fit = flu_coxmod, transform = "identity")
   # calculate VE
-    temp <- durham_ve(flu_zph, var = "V") %>% mutate(Sim = i)
+    temp <- durham_ve(flu_zph, var = "V") %>% mutate(Sim = i, Method = "Durham")
     if (i > 1){
     ve_est <- bind_rows(ve_est,temp)
     } else {ve_est <- temp}
+    
+    ######################################
+    ### method from Tian et al. 2005 ###
+    ######################################
+    # calculate VE
+    temp2 <- tian_ve(outcomes_dat1) %>% mutate(Sim = i, Method = "Tian")
+      ve_est <- bind_rows(ve_est,temp2)
 }
-# subset for 1 sim to plot
-ve_est1 <- ve_est %>% filter(Sim == 1)
-# plot VE
-y_min <- ifelse(min(ve_est1$y_lower) < 0, min(ve_est1$y_lower), 0)
-y_max <- ifelse(max(ve_est1$y_upper) > 1, max(ve_est1$y_upper), 1)
 
-p_durham <- ggplot(data = ve_est1, aes(x = pred_x, y = y_hat)) + geom_line() +
-            geom_ribbon(aes(x = pred_x, ymin = y_lower, ymax = y_upper, linetype = NA), alpha=0.2) +
-            xlab("Day") + ylab("VE") + scale_y_continuous(limits = c(y_min, y_max)) +
-            theme(panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_blank(),
-                  axis.line = element_line(colour = "black"))
-p_durham
+# mean VE from simulations
+mean_ve <- ve_est %>% group_by(Method, time) %>% summarise_at("ve", c(mean, sd)) %>% rename(ve_mean = fn1, ve_sd = fn2)
+mean_ve
 
-### apply method from Tian et al. 2005 (JASA)
-ve_est2 <- tian_ve(dat)
 
-### apply method from Ainslie et al. 2017 (SIM)
+### ignore this for now ###
+
+# plot of mean VE estimates over time
+# ve_est1 <- ve_est %>% filter(Method == "Durham")
+# # plot VE
+# y_min <- ifelse(min(ve_est1$y_lower) < 0, min(ve_est1$y_lower), 0)
+# y_max <- ifelse(max(ve_est1$y_upper) > 1, max(ve_est1$y_upper), 1)
+# 
+# p_durham <- ggplot(data = ve_est1, aes(x = pred_x, y = y_hat)) + geom_line() +
+#             geom_ribbon(aes(x = pred_x, ymin = y_lower, ymax = y_upper, linetype = NA), alpha=0.2) +
+#             xlab("Day") + ylab("VE") + scale_y_continuous(limits = c(y_min, y_max)) +
+#             theme(panel.grid.major = element_blank(),
+#                   panel.grid.minor = element_blank(),
+#                   panel.background = element_blank(),
+#                   axis.line = element_line(colour = "black"))
+# p_durham
+
+
+
+
 
