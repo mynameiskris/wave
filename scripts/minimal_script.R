6c7a794bec2a0a2d83f2cb487a9cff1e9e835df0
kylieainslie <ainslie.kylie@gmail.com>
2020-02-25 07:58:12 +0000

    add more instructions

--- scripts/minimal_script.R
+++ scripts/minimal_script.R
@@ -8,16 +8,17 @@ library(timereg)
 library(ggplot2)
 
 ### Source functions from other files
-source('R/simvee.R')
-source('R/readParams.R')
-source('R/ve_methods.R')
+source('simvee.R')
+source('readParams.R')
+source('ve_methods.R')
 
 ### Read parameters from input files
 #   you can specify the folder and file names of the input file within the ""
-params <- readParams("input/SimVEE_input_Test_05.csv")
+#   if no path is specified a window will pop up and allow you to choose a file from your computer
+params <- readParams("SimVEE_input_Test_05.csv")
 
 ### run simulation
-#   there is an optional path argument for run_sim(params, path = )
+#   there is an optional path argument for run_simvee(params, path = )
 #   if no path is specified, it will default to current working directory
 outcomes_dat <- run_simvee(params)
 
